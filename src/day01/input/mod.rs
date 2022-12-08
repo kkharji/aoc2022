1eb78b0b7b619e8b42338f34407cec85f64c1372
kkharji <kkharji@protonmail.com>
2022-12-08 10:47:38 +0300

    day(1): solve part 1

--- src/day01/input/mod.rs
+++ src/day01/input/mod.rs
@@ -5,7 +5,23 @@ impl GetInput for Day01 {
     type Input = super::Input;
 
     fn text_to_input(content: &str) -> Self::Input {
-        todo!()
+        let mut elfs: Self::Input = vec![];
+        let mut lines = content.split("\n");
+        let mut current_elf = 0;
+
+        while let Some(cal) = lines.next() {
+            if cal.is_empty() {
+                current_elf += 1;
+                continue;
+            }
+            let val = cal.parse().expect("number found something else");
+            if let Some(elem) = elfs.get_mut(current_elf) {
+                elem.push(val)
+            } else {
+                elfs.push(vec![val])
+            }
+        }
+        elfs
     }
 
     fn data() -> Self::Input {
@@ -19,4 +35,7 @@ impl GetInput for Day01 {
 
 #[test]
 fn test_parse_example() {
+    let output = Day01::example();
+    println!("{output:#?}");
+    assert_eq!(output[1], vec![4000])
 }
--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -7,15 +7,31 @@ pub struct Day01;
 
 type Input = Vec<Vec<i32>>;
 
-impl Solve for Day01 {}
+impl Solve for Day01 {
+    fn part1() -> crate::Output {
+        Day01::data()
+            .into_iter()
+            .map(|cals| cals.into_iter().sum::<i32>())
+            .max()
+            .unwrap_or_default()
+            .into()
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    fn check_example() {
+        let result = Day01::example().iter().map(|cals| cals.iter().sum()).max();
+        assert_eq!(result, Some(24000));
+    }
+
+    #[test]
     fn check_part1() {
-        let _result = Day01::part1();
+        let result = Day01::part1();
+        assert_eq!(result, 70296)
     }
 
     #[test]
