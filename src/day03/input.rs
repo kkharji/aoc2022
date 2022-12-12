cbd3d7eafd3e01a666e69ee57ccb4ec7cd707229
kkharji <kkharji@protonmail.com>
2022-12-12 08:52:34 +0300

    day(03): parse input

--- src/day03/input.rs
+++ src/day03/input.rs
@@ -1,18 +1,34 @@
+use std::ops::Div;
+
 use super::{Case, GetInput};
 
 impl GetInput for Case {
-    type Input = ();
+    type Input = Vec<(String, String)>;
 
     const NAME: &'static str = "day03";
 
-    fn text_to_input(_content: &str) -> Self::Input {
-        ()
+    fn text_to_input(content: &str) -> Self::Input {
+        content
+            .split("\n")
+            .filter(|line| !line.is_empty())
+            .map(|line| line.split_at(line.len().div(2)))
+            .map(|(a, b)| (a.to_string(), b.to_string()))
+            .collect()
     }
 }
 
 #[test]
 fn test_parse_example() {
     let output = Case::example();
-    println!("{output:#?}");
-    assert_eq!(output, ())
+    let expected = [
+        ("vJrwpWtwJgWr", "hcsFMMfFFhFp"),
+        ("jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL"),
+        ("PmmdzqPrV", "vPwwTWBwg"),
+        ("wMqvLMZHhHMvwLH", "jbvcjnnSBnvTQFn"),
+        ("ttgJtRGJ", "QctTZtZT"),
+        ("CrZsJsPPZsGz", "wwsLwLmpwMDw"),
+    ]
+    .map(|(a, b)| (a.to_string(), b.to_string()))
+    .to_vec();
+    assert_eq!(output, expected)
 }
